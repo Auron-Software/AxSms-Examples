5bf87d5c1783b9524b25a30913acb11107e461d8
Sirp Potijk <s.potijk@auronsoftware.com>
2022-06-22 13:37:32 +0200

    Update according to later version

--- Visual C++/Include/AxSms_i.h
+++ Visual C++/Include/AxSms_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Mar 23 04:11:23 2022
+/* at Sun Jun 19 23:40:22 2022
  */
 /* Compiler settings for AxSms.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
